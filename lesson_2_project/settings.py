e7fcb3edc21b8184edf3616992d3df3d26bc9672
Codemind <walter.indeche@proton.me>
2024-11-07 11:36:13 +0300

    Handling static files

--- lesson_2_project/settings.py
+++ lesson_2_project/settings.py
@@ -119,6 +119,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static_files' # main directory - created automatically
+
+STATICFILES_DIRS = [BASE_DIR / 'main_app/static'] # each app in the project
+
+# python manage.py collectstatic
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
